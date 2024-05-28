e845721a5dbbb8f8665000c57480879438cabda6
AnonimowyBanan <halama.robert497@gmail.com>
2024-05-28 20:21:23 +0200

    feat: budget migration

--- database/migrations/2024_05_28_181126_create_budgets_table.php
+++ database/migrations/2024_05_28_181126_create_budgets_table.php
@@ -4,13 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('transactions', function (Blueprint $table) {
+        Schema::create('budgets', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')
@@ -22,13 +23,9 @@ return new class extends Migration {
                 ->references('id')
                 ->on('transaction_categories')
                 ->onDelete('cascade');
-            $table->unsignedBigInteger('type_id');
-            $table->foreign('type_id')
-                ->references('id')
-                ->on('transaction_types')
-                ->onDelete('cascade');
             $table->decimal('amount');
-            $table->date('transaction_date');
+            $table->date('start_date');
+            $table->date('end_date');
             $table->timestamps();
         });
     }
@@ -38,11 +35,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table('transactions', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
-            $table->dropForeign(['category_id']);
-            $table->dropForeign(['type_id']);
-        });
-        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('budgets');
     }
 };
