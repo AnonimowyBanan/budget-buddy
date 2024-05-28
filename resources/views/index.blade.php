5f032f5a5047f29db4e7c20d38f5b4f6f2401fa4
AnonimowyBanan <halama.robert497@gmail.com>
2024-05-28 21:27:46 +0200

    fix: diasyui components load

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('base')
+
+@section('content')
+    <button class="btn">Button</button>
+@endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './resources/**/*.{php, js}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+      require('daisyui'),
+  ],
 }
 
